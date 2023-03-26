422e91d76ea0ec8668a37e23d93cff80c853106d
Ashutosh Gupta <ashutoshgupta@Ashutoshs-MacBook-Air.local>
2023-03-26 20:38:48 +0530

    first commit

---
+++ Media/IPL 2023_ Impact Player, 2 team sheets! Harsha Bhogle decodes all the new rules _ Cricbuzz.com_files/verify.js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_194347952707({ImpressionID:"d169fd200ad443cf885d67b1cd576f0a", ServerPublicDns:"rtbc-ae1.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "d169fd200ad443cf885d67b1cd576f0a");}catch(e){}
+try{__verify_callback_194347952707({
+ResultID:2,
+Passback:"",
+AdWidth:728,
+AdHeight:90});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "d169fd200ad443cf885d67b1cd576f0a");}catch(e){}
